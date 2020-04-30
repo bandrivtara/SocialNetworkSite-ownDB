9be2a0646e01611df5f6f57de60d1f33146b3d84
Taras Bandrivskyy <bandrivtara@gmail.com>
2020-04-30 22:35:15 +0200

    Updates

---
+++ precache-manifest.3b6cc06aa782561d9bef29e85143be9a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be2e1e424bdc6c2fe6f5aa11653e81f",
+    "url": "/SocialNetworkSite/index.html"
+  },
+  {
+    "revision": "d8a9c5956d256fa9ddd3",
+    "url": "/SocialNetworkSite/static/css/3.b3d03572.chunk.css"
+  },
+  {
+    "revision": "4e11a4fd563c33c7f4f4",
+    "url": "/SocialNetworkSite/static/css/main.f2910b0d.chunk.css"
+  },
+  {
+    "revision": "5afe58314b78a91482d8",
+    "url": "/SocialNetworkSite/static/js/2.5449160e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/SocialNetworkSite/static/js/2.5449160e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8a9c5956d256fa9ddd3",
+    "url": "/SocialNetworkSite/static/js/3.38cfeaaf.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/SocialNetworkSite/static/js/3.38cfeaaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e11a4fd563c33c7f4f4",
+    "url": "/SocialNetworkSite/static/js/main.c02751ea.chunk.js"
+  },
+  {
+    "revision": "cfebfb887bef6540c6d0",
+    "url": "/SocialNetworkSite/static/js/runtime-main.fe58fd38.js"
+  },
+  {
+    "revision": "efcd42d63b6f7e8ab3428b6dbd7c7fc1",
+    "url": "/SocialNetworkSite/static/media/noUser.efcd42d6.png"
+  },
+  {
+    "revision": "e73ca6ae6ba1d68feb99125e4873b8f7",
+    "url": "/SocialNetworkSite/static/media/old-photos-in-the-wooden-box-5842.e73ca6ae.jpg"
+  },
+  {
+    "revision": "92e274c74c3a84489a1d343c30f55f21",
+    "url": "/SocialNetworkSite/static/media/preloader.92e274c7.svg"
+  }
+]);
